cd05187079b59ec723caaf3e32a0043dfcfcf47e
krolchaves <155522077+krolchaves@users.noreply.github.com>
2025-08-15 12:02:09 -0300

    Exercício Calculadora JS

---
+++ calculadora.js
@@ -0,0 +1,12 @@
+// Exercício - Calculadora //
+
+let num1 = 30
+let num2 = 10
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1/num2
+console.log("A soma é: ", soma)
+console.log("A subtração é: ", subtracao)
+console.log("A multiplicação é: ", multiplicacao)
+console.log("A divisão é: ", divisao)
